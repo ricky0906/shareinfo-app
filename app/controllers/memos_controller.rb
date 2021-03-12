43b36f9bc6a9733009cce9b34816062ae2fd4acf
ricky0906 <rupinia.ricky@gmail.com>
2021-03-12 16:38:10 +0900

    メッセージ送信機能の実装

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -1,4 +1,23 @@
 class MemosController < ApplicationController
   def index
+    @memo = Memo.new
+    @room = Room.find(params[:room_id])
+    @rooms = Room.all
+  end
+
+  def create
+    @room = Room.find(params[:room_id])
+    @memo = @room.memos.new(memo_params)
+    if @memo.save
+      redirect_to room_memos_path(@room)
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def memo_params
+    params.require(:memo).permit(:content).merge(user_id: current_user.id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'memos/index'
   root to: "rooms#index"
   resources :users, only: [:edit, :update]
-  resources :rooms
+  resources :rooms do
+    resources :memos, only: [:index, :create]
+  end
 end
