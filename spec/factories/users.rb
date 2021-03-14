dccff89753d4d9de5805c1c36665be44b974154c
ricky0906 <rupinia.ricky@gmail.com>
2021-03-14 10:53:10 +0900

    各モデルのテストとエラーハンドリング

---
+++ spec/factories/users.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :user do
+    nickname                { Faker::Name.initials(number: 2) }
+    first_name              { Faker::Name.first_name }
+    last_name               { Faker::Name.last_name }
+    email                   { Faker::Internet.free_email }
+    password                { '1a' + Faker::Internet.password(min_length: 4) }
+    password_confirmation   {password}
+  end
+end
